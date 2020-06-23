dbee0c6f5133699d17cfb5561c998cee7378d557
fede2s <fede.2santos_96@hotmail.com>
2020-06-23 12:35:39 -0300

    Puliendo detalles

--- test.py
+++ test.py
@@ -19,11 +19,11 @@ mensajeTipoDeTest+="3) Ho: X<=Xo\t\tHa: X>Xo\t (Si la media es menor a ...)\n"
 tipoDeTest=int(input(mensajeTipoDeTest))
 
 while tipoDeTest!=1 and tipoDeTest!=2 and tipoDeTest!=3:
-    tipoDeTest=int(input("Ingreso no válido\nIngrese 1, 2 o 3 segun el test de hipotesis a realizar"))
+    tipoDeTest=int(input("Ingreso no válido\nIngrese 1, 2 o 3 segun el test de hipotesis a realizar: "))
 
 os.system('cls')
 
-significacion=float(input("Ingrese el nivel de significacion"))
+significacion=float(input("Ingrese el nivel de significacion: "))
 
 if tipoDeTest==1:
     limiteInferior=significacion/2
@@ -50,21 +50,36 @@ z=(mediahip-media)/(desvio/math.sqrt(n))
 
 
 
-
+#La probabilidad acumulada de Zobs con Z~N(mu=hipotesis,sigma=desvíomuestral/raiz(totalDePoblacion))
 pro=scipy.stats.norm(media,desvio/math.sqrt(n)).cdf(mediahip)
 
+#Borro la consola porque tengo mucho para imprimir
 os.system('cls')
 
+#Muestro qué pasa con Zobs y el intervalo de aceptación
 print('El Z observado da =',z,"\n\tZ(obs) =", z,'\n')
 
+num1='{:.2f}'.format(norm.ppf(limiteInferior))
+
+num2='{:.2f}'.format(norm.ppf(limiteSuperior))
+
+print('El intervalo de aceptación es [',num1,':',num2,']. Si Zobs está fuera, se rechaza la hipotesis\n')
+
+#Muestro la distribución normal de Z
 print('Z distribuye de forma normal con media=',media,' y desvio=',desvio/math.sqrt(n),')\n\t'"Z ~ N (",media,",",desvio/math.sqrt(n),')\n')
 
+#Zobs tiene una acumulada y los límites del intervalo de aceptación también tienen una probabilidad acumulada
+#Si la probabilidad de Zobs se encuentra fuera de la probabilidad de los límites de aceptación, es porque Zobs
+    #se encuentra fuera de los intervalos de aceptación
 print('Probabilidad de Z observado\n\tP(Zobs)=', pro, '\n')
 
-print('El intervalo de probabilidad de aceptación es [',norm.ppf(limiteInferior),':',norm.ppf(limiteSuperior),']. Si Zobs está fuera, se rechaza la hipotesis\n')
+print('El intervalo de aceptación tiene las siguientes probabilidades acumuladas [',limiteInferior,':',limiteSuperior,']\nSi la probabilidad acumulada de Zobs está fuera, se rechaza la hipótesis.\n\n')
 
+print('-------------------------------------Resultado--------------------------------------------')
 
 if pro<limiteSuperior and pro>limiteInferior:
+    print('Zobs =',z,' se encuentra DENTRO del intervalo de aceptación [',num1,':',num2,'], entonces...')
     print("No hay evidencia suficiente para rechazar la hipotesis")
 else:
+    print('Zobs =',z,' se encuentra FUERA del intervalo de aceptación [',num1,':',num2,'], entonces...')
     print("Con un test de hipotesis con nivel de signficiacion",significacion, "hay evidencia suficiente para rechazar la hipotesis")
